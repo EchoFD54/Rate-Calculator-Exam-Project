b15b6aaa4a04a5a925a134a92a597dc3df81c167
EchoFD54 <144889612+EchoFD54@users.noreply.github.com>
2024-05-05 09:13:13 +0200

    checks for numbers

--- src/GUI/controller/AddEmployeeController.java
+++ src/GUI/controller/AddEmployeeController.java
@@ -3,6 +3,7 @@ package GUI.controller;
 import BE.Employee;
 import BLL.EmployeeManager;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
@@ -36,14 +37,47 @@ public class AddEmployeeController {
             isOverheadCost = false;
         }
 
+        // Check if all input are valid numbers
+        boolean isValidInput = true;
+        String invalidField = "";
 
-        //add employee to database and tableview
-       mainWindowController.updateEmployeeProperties(name, annSalary, multPer, fixedAnnAmt, country, team, workHours, utilization, isOverheadCost);
+        if (!isValidDouble(annSalary)) {
+            isValidInput = false;
+            invalidField = "Annual Salary";
+        } else if (!isValidDouble(multPer)) {
+            isValidInput = false;
+            invalidField = "Multiplier Percentage";
+        } else if (!isValidDouble(fixedAnnAmt)) {
+            isValidInput = false;
+            invalidField = "Fixed Annual Amount";
+        } else if (!isValidDouble(workHours)) {
+            isValidInput = false;
+            invalidField = "Work Hours";
+        } else if (!isValidDouble(utilization)) {
+            isValidInput = false;
+            invalidField = "Utilization";
+        }
 
-        ((Stage) empNameField.getScene().getWindow()).close();
+        if (isValidInput){
+            // Add employee to database and tableview if input is valid
+            mainWindowController.updateEmployeeProperties(name, annSalary, multPer, fixedAnnAmt, country, team, workHours, utilization, isOverheadCost);
+            ((Stage) empNameField.getScene().getWindow()).close();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Input Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Please input a valid number for " + invalidField);
+            alert.showAndWait();
+        }
+    }
 
-
-
+    private boolean isValidDouble(String value) {
+        try {
+            Double.parseDouble(value);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 
     public void setMainWindowController(MainWindowController mainWindowController) {
