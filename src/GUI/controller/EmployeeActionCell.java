139b21ce1c503c7e76ba25ec82db8182aef24ab1
EchoFD54 <144889612+EchoFD54@users.noreply.github.com>
2024-05-22 14:31:59 +0200

    fiInal Touches for review

--- src/GUI/controller/EmployeeActionCell.java
+++ src/GUI/controller/EmployeeActionCell.java
@@ -4,23 +4,58 @@ import BE.Employee;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.util.Callback;
 
 public class EmployeeActionCell extends TableCell<Employee, Void> {
     private final HBox actionButtons;
-
     private final Button editButton;
     private final Button deleteButton;
+    private final Button addToTeamButton;
+    private final Button removeFromTeamButton;
+
     private final EmployeeWindowController controller;
 
 
     public EmployeeActionCell(EmployeeWindowController controller) {
         this.controller = controller;
-        this.editButton = new Button("Edit");
-        this.deleteButton = new Button("Delete");
-        this.actionButtons = new HBox(10, editButton, deleteButton);
 
+
+
+
+        // Load images
+        Image editImage = new Image(getClass().getResourceAsStream("/GUI/view/Images/edit_icon.png"));
+        Image deleteImage = new Image(getClass().getResourceAsStream("/GUI/view/Images/delete_icon.png"));
+        Image addToTeamImage = new Image(getClass().getResourceAsStream("/GUI/view/Images/addToTeam_icon.png"));
+        Image removeImage = new Image(getClass().getResourceAsStream("/GUI/view/Images/removeFromTeam_icon.png"));
+        ImageView editImageView = new ImageView(editImage);
+        ImageView deleteImageView = new ImageView(deleteImage);
+        ImageView addToTeamImageView = new ImageView(addToTeamImage);
+        ImageView removeFromTeamImageView = new ImageView(removeImage);
+        editImageView.setFitWidth(25);
+        editImageView.setFitHeight(25);
+        deleteImageView.setFitWidth(25);
+        deleteImageView.setFitHeight(25);
+        addToTeamImageView.setFitWidth(25);
+        addToTeamImageView.setFitHeight(25);
+        removeFromTeamImageView.setFitWidth(25);
+        removeFromTeamImageView.setFitHeight(25);
+
+        this.editButton = new Button();
+        this.editButton.setGraphic(editImageView);
+
+        this.deleteButton = new Button();
+        this.deleteButton.setGraphic(deleteImageView);
+
+        this.addToTeamButton = new Button();
+        this.addToTeamButton.setGraphic(addToTeamImageView);
+
+        this.removeFromTeamButton = new Button();
+        this.removeFromTeamButton.setGraphic(removeFromTeamImageView);
+
+        this.actionButtons = new HBox(10, editButton, deleteButton, addToTeamButton, removeFromTeamButton);
 
         editButton.setOnAction(event -> {
             Employee currentEmployee = getTableView().getItems().get(getIndex());
